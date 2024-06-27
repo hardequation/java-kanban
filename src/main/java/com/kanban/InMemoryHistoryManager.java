bd57736744599bef4a5350c689f74a811d41bc4a
Altana Batueva <altana.batueva0@gmail.com>
2024-06-27 23:45:12 +0200

    add remove method to implementation class

--- src/main/java/com/kanban/InMemoryHistoryManager.java
+++ src/main/java/com/kanban/InMemoryHistoryManager.java
@@ -2,28 +2,39 @@ package com.kanban;
 
 import com.kanban.tasks.Task;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int HISTORY_SIZE = 10;
-    private final Deque<Task> history;
+    private final LinkedHashMap<Integer,Task> history;
 
     public InMemoryHistoryManager() {
-        this.history = new ArrayDeque<>(HISTORY_SIZE);
+        this.history = new LinkedHashMap<>(HISTORY_SIZE, 0.75f, true);
     }
 
     @Override
     public void add(Task task) {
-        if (history.size() >= HISTORY_SIZE) {
-            history.removeFirst();
+        if (history.containsKey(task.getId())) {
+            history.replace(task.getId(), task);
+            return;
         }
-        history.add(task);
+
+        if (history.size() >= HISTORY_SIZE) {
+            history.pollFirstEntry();
+        }
+
+        history.put(task.getId(), task);
     }
 
     @Override
     public List<Task> getHistory() {
-        return history.stream().toList();
+        return history.values().stream().toList();
+    }
+
+    @Override
+    public void remove(int id) {
+        history.remove(id);
     }
 }
