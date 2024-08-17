1459e96ecdb25e3d046ae722a7bf3529bba6e55c
Altana Batueva <altana.batueva0@gmail.com>
2024-08-18 01:26:11 +0200

    fix style

--- src/main/java/com/kanban/client/BaseHttpHandler.java
+++ src/main/java/com/kanban/client/BaseHttpHandler.java
@@ -17,10 +17,11 @@ public abstract class BaseHttpHandler implements HttpHandler {
     protected static final int NOT_ACCEPTABLE = 406;
 
     protected static final int INTERNAL_SERVER_ERROR = 500;
-    
+
     private static final String CONTENT_TYPE = "Content-Type";
 
     private static final String CONTENT_TYPE_VALUE = "application/json;charset=utf-8";
+
     protected void sendText(HttpExchange h, String text, int statusCode) throws IOException {
         byte[] resp = text.getBytes(StandardCharsets.UTF_8);
         h.getResponseHeaders().add(CONTENT_TYPE, CONTENT_TYPE_VALUE);
@@ -32,7 +33,7 @@ public abstract class BaseHttpHandler implements HttpHandler {
     protected Integer getIdFromPath(String path) {
         String[] parts = path.split("/");
         if (parts.length >= 3) {
-            return Integer.parseInt(parts[2 ]);
+            return Integer.parseInt(parts[2]);
         }
         return null;
     }
